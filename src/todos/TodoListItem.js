cd6f2571e9ab45dfce8bf516d616ec8914ff3597
SamPCunningham <51377712+SamPCunningham@users.noreply.github.com>
2021-03-03 15:21:13 -0500

    converted to styled components

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,22 +1,56 @@
 import React from 'react';
-import './TodoListItem.css';
+import styled from 'styled-components';
+
+const CompletedButton = styled.button`
+    font-size: 13px;
+    padding: 8px;
+    cursor: pointer;
+    background-color: #54b948;
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const RemoveButton = styled.button`
+    font-size: 13px;
+    padding: 8px;
+    cursor: pointer;
+    background-color: #e31836;
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const TodoItemContainer = styled.div`
+    border: 1px solid;
+    border-color: #54b948;
+    border-radius: 10px 10px 10px 0px;
+    margin-top: 8px;
+    padding: 16px;
+    position: relative;
+    box-shadow: 0 4px 8px grey;
+`;
+
+const ButtonsContainer = styled.div`
+    position: absolute;
+    right: 12px;
+    bottom: 12px;
+`;
 
 const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
-    <div className="todo-item-container">
+    <TodoItemContainer>
         <h3>{todo.text}</h3>
-        <div className="buttons-container">
+        <ButtonsContainer>
             {todo.isCompleted
-            ? <p>Item Complete</p>
-            : <button 
+            ? null
+            : <CompletedButton
                 onClick={() => onCompletedPressed(todo.id)}
-                className="completed-button">Mark As Completed</button>
+                className="completed-button">Mark As Completed</CompletedButton>
             }
             
-            <button 
+            <RemoveButton 
             onClick={() => onRemovePressed(todo.id)}
-            className="remove-button">Remove</button>
-        </div>
-    </div>
+            className="remove-button">Remove</RemoveButton>
+        </ButtonsContainer>
+    </TodoItemContainer>
 )
 
 export default TodoListItem;
